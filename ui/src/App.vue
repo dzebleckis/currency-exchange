8191dbce3b5a88b39712e27c936bef14dc242028
Darius Zebleckis <darius.zebleckis@lamoda.ru>
2019-12-20 00:29:16 +0200

    WIP: ui cleanup

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <form>
+      <label> From
+        <CurrencyList :currencies=currencies />
+      </label>
+    </form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import CurrencyList from './components/CurrencyList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CurrencyList
+  },
+  data: function () {
+    return { currencies: ["EUR", "USD"] }
   }
 }
 </script>
@@ -21,7 +30,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ ui/src/components/CurrencyList.vue
@@ -0,0 +1,17 @@
+<template>
+  <select >
+    <option v-for="currency in currencies" :key="currency" >{{ currency }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'CurrencyList',
+  props: {
+    currencies: Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
